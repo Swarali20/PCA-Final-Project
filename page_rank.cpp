5d15bef13459bc7a7355f8503ae2f59afc8e3821
aditya bhawkar <adityabhawkar@adb97.fios-router.home>
2021-12-02 22:40:43 -0500

    Basic serial implementation working for small graph

--- page_rank.cpp
+++ page_rank.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <list>
 #include <map>
-#include <omp.h>
+//#include <omp.h>
 #include <time.h>
 
 using namespace std;
@@ -44,7 +44,7 @@ int main(int argc, char** argv){
     }
 
     char *graph_filename = argv[1];
-    int num_threads = atoi(argv[2]);
+    int damping = atoi(argv[2]);
 
     // vector <Page> pages;
     map <int, Page> pages;
@@ -65,6 +65,7 @@ int main(int argc, char** argv){
               pages[from_idx].num_in_pages = 0;
               pages[from_idx].num_out_pages = 0;
               pages[from_idx].page_rank = 0;
+              lookup[num_pages] = from_idx;
               num_pages++;
           }
           if (!pages.count(to_idx)) {
@@ -72,15 +73,42 @@ int main(int argc, char** argv){
               pages[to_idx].num_in_pages = 0;
               pages[to_idx].num_out_pages = 0;
               pages[to_idx].page_rank = 0;
+              lookup[num_pages] = to_idx;
               num_pages++;
           }
 
           pages[from_idx].num_out_pages++;
           pages[to_idx].num_in_pages++;
           pages[to_idx].incoming_ids.push_back(from_idx);
-        
       }
    }
-   fclose(fid);
+    float initialize_score = 1.0 / num_pages;
+    for(auto& x : pages)
+        x.second.page_rank = initialize_score;
+
+    int num_iterations = 5;
+    int idx;
+    for (int iter = 0; iter < num_iterations;iter++){
+        for (int i = 0; i<pages.size();i++){
+            cout << "Node index: " << lookup[i] << endl;
+            idx = lookup[i];
+            pages[idx].temp_page_rank = 0.0;
+            if (pages[idx].num_in_pages>0){
+                int num_incoming = pages[idx].incoming_ids.size();
+                // cout << "Incoming: ";
+                for (int j = 0 ; j<num_incoming;j++){
+
+                    int in_id = pages[idx].incoming_ids[j];
+                    pages[idx].temp_page_rank += pages[in_id].page_rank / pages[in_id].num_out_pages;
+                }
+            }
+            pages[idx].page_rank = (1.0 - damping) * pages[idx].temp_page_rank + damping / num_pages;
+            cout << "New page rank: " << pages[idx].page_rank << endl;
+            cout << endl;
+
+        }
+    }
+
+    fclose(fid);
 
 }
